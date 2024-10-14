38d90e5de2e7f014c65eff0ef59e24ed19c3a280
Nghia La <leonghiacnn@gmail.com>
2024-10-14 11:49:14 +0000

    Add Welcome page

--- app/src/Router.tsx
+++ app/src/Router.tsx
@@ -1,11 +1,16 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { HomePage } from './pages/Home.page';
+import WelcomePage from './pages/Welcome.page';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <HomePage />,
   },
+  {
+    path: '/welcome',
+    element: <WelcomePage />
+  }
 ]);
 
 export function Router() {
