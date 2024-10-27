0a4dff5a05d5a35aa0db871885a07307acbd9c40
Nghia La <leonghiacnn@gmail.com>
2024-10-27 06:25:33 +0000

    Implement Framer motion on Welcome page

--- app/src/components/Welcome/Welcome.tsx
+++ app/src/components/Welcome/Welcome.tsx
@@ -1,13 +1,88 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import classes from './Welcome.module.scss';
+import { AnimatePresence, motion } from 'framer-motion';
 import { useNavigate } from 'react-router-dom';
 import { Button } from '@mantine/core';
-import { useLocalStorage, useTimeout } from '@mantine/hooks';
+import { useLocalStorage } from '@mantine/hooks';
 import centerIllustrator from '../../assets/images/hello_illustrator.webp';
 import logo from '../../assets/images/logo_1.svg';
 import leftSideIllustrator from '../../assets/images/side_illustrators_1.webp';
 import rightSideIllustrator from '../../assets/images/side_illustrators_2.webp';
 import EnterName from './EnterName/EnterName';
+
+type NameSectionProps = { onNext: (value: string) => void };
+
+const NameSection = React.forwardRef<HTMLElement, NameSectionProps>((props, ref) => {
+  return (
+    <section ref={ref} className={classes.nameSection}>
+      {/* Logo */}
+      <figure className={classes.logo}>
+        <img src={logo} alt="carshare logo" />
+      </figure>
+      {/* Enter name */}
+      <EnterName onNext={props.onNext} />
+    </section>
+  );
+});
+
+const MotionNameSection = motion.create(NameSection);
+
+type WelcomeSectionProps = { name: string; onClick: () => void };
+
+const WelcomeSection = React.forwardRef<HTMLElement, WelcomeSectionProps>((props, ref) => {
+  const variants = {
+    '0%': {
+      opacity: 0,
+      y: '-100%',
+    },
+    '50%': {
+      opacity: 1,
+      y: '20%',
+    },
+    '75%': {
+      opacity: 1,
+      y: '-10%',
+    },
+    '100%': {
+      opacity: 1,
+      y: 0,
+    },
+  };
+
+  return (
+    <section ref={ref} className={classes.welcomeSection}>
+      {/* Logo */}
+      <figure className={classes.logo}>
+        <img src={logo} alt="carshare logo" />
+      </figure>
+      {/* Center illustrator */}
+      <motion.figure
+        animate={{
+          opacity: [0, 1, 1, 1],
+          y: ['-100%', '20%', '-10%', 0],
+        }}
+        transition={{ duration: 1, ease: 'easeOut', delay: 0.4 }}
+        className={classes.centerIllustrator}
+      >
+        <img src={centerIllustrator} alt="center illustrator" />
+      </motion.figure>
+      {/* Hello text & Welcome text */}
+      <div className={classes.helloAndWelcomeText}>
+        <p className={classes.helloText}>Chào {props.name}!</p>
+        <h1 className={classes.welcomeText}>Chào mừng bạn đến với CarShare</h1>
+      </div>
+      {/* Start button */}
+      <Button
+        classNames={{ root: classes.startButtonRoot, label: classes.buttonLabel }}
+        onClick={props.onClick}
+      >
+        Bắt đầu
+      </Button>
+    </section>
+  );
+});
+
+const MotionWelcomeSection = motion.create(WelcomeSection);
 
 export default function Welcome() {
   const navigate = useNavigate();
@@ -16,16 +91,12 @@ export default function Welcome() {
   const [userName, setUserName] = useLocalStorage<string>({
     key: 'user_name',
   });
-  const { start, clear } = useTimeout(() => setIsWelcomeVisible(true), 350);
-  const [isAnimated, setIsAnimated] = useState<boolean>(false);
 
   const handleNext = (value: string) => {
     setName(value);
     // Save name to localStorage
     setUserName(value);
-
-    setIsAnimated(true);
-    start();
+    setIsWelcomeVisible(true);
   };
 
   const handleStart = () => {
@@ -35,67 +106,36 @@ export default function Welcome() {
   return (
     <div className={classes.container}>
       {/* Left side illustrator */}
-      <figure className={classes.sideIllustrator + ' ' + classes.sideIllustratorLeft}>
+      <figure key="left" className={classes.sideIllustrator + ' ' + classes.sideIllustratorLeft}>
         <img src={leftSideIllustrator} alt="left side illustrator" />
       </figure>
       {/* Section */}
-      {isWelcomeVisible ? (
-        <WelcomeSection onClick={handleStart} name={name} />
-      ) : (
-        <NameSection isSlideOut={isAnimated} onNext={handleNext} />
-      )}
+      <AnimatePresence mode="wait">
+        {isWelcomeVisible && (
+          <MotionWelcomeSection
+            key="welcome"
+            initial={{ opacity: 0, visibility: 'hidden', x: '100%' }}
+            animate={{ opacity: 1, visibility: 'visible', x: 0 }}
+            transition={{ duration: 0.3, ease: 'easeOut' }}
+            onClick={handleStart}
+            name={name}
+          />
+        )}
+        {!isWelcomeVisible && (
+          <MotionNameSection
+            key="name"
+            initial={{ opacity: 1, visibility: 'visible', x: 0 }}
+            exit={{ opacity: 0, visibility: 'hidden', x: '-100%' }}
+            transition={{ duration: 0.3, ease: 'easeOut' }}
+            onNext={handleNext}
+          />
+        )}
+      </AnimatePresence>
+
       {/* Right side illustrator */}
-      <figure className={classes.sideIllustrator + ' ' + classes.sideIllustratorRight}>
+      <figure key="right" className={classes.sideIllustrator + ' ' + classes.sideIllustratorRight}>
         <img src={rightSideIllustrator} alt="right side illustrator" />
       </figure>
     </div>
-  );
-}
-
-function NameSection({
-  onNext,
-  isSlideOut,
-}: {
-  onNext: (value: string) => void;
-  isSlideOut: boolean;
-}) {
-  return (
-    <section
-      className={isSlideOut ? classes.nameSection + ' ' + classes.slideOut : classes.nameSection}
-    >
-      {/* Logo */}
-      <figure className={classes.logo}>
-        <img src={logo} alt="carshare logo" />
-      </figure>
-      {/* Enter name */}
-      <EnterName onNext={onNext} />
-    </section>
-  );
-}
-
-function WelcomeSection({ name, onClick }: { name: string; onClick: () => void }) {
-  return (
-    <section className={classes.welcomeSection + ' ' + classes.slideIn}>
-      {/* Logo */}
-      <figure className={classes.logo}>
-        <img src={logo} alt="carshare logo" />
-      </figure>
-      {/* Center illustrator */}
-      <figure className={classes.centerIllustrator + ' ' + classes.falling}>
-        <img src={centerIllustrator} alt="center illustrator" />
-      </figure>
-      {/* Hello text & Welcome text */}
-      <div className={classes.helloAndWelcomeText}>
-        <p className={classes.helloText}>Chào {name}!</p>
-        <h1 className={classes.welcomeText}>Chào mừng bạn đến với CarShare</h1>
-      </div>
-      {/* Start button */}
-      <Button
-        classNames={{ root: classes.startButtonRoot, label: classes.buttonLabel }}
-        onClick={onClick}
-      >
-        Bắt đầu
-      </Button>
-    </section>
   );
 }
