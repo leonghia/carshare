942a9cb19d1dd5c354f0c9814d903ac067aad035
Nghia La <leonghiacnn@gmail.com>
2024-12-28 16:22:46 +0000

    Adjust styling for Label

--- client/src/components/ui/label.tsx
+++ client/src/components/ui/label.tsx
@@ -7,7 +7,7 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 const labelVariants = cva(
-  "w-full block font-normal group-focus-within:text-primary-500",
+  "block font-normal group-focus-within:text-primary-500",
   {
     variants: {
       size: {
