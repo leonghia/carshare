0cb88d96a7845440790db6fc83d6892cb6b2d7f1
Nghia La <leonghiacnn@gmail.com>
2025-02-09 09:09:45 +0000

    Create config file

---
+++ client/src/lib/config.ts
@@ -0,0 +1,3 @@
+export const GGMAPS_API_KEY = import.meta.env.VITE_GGMAPS_API_KEY;
+export const GGMAPS_MAPTILES_KEY = import.meta.env.VITE_GGMAPS_MAPTILES_KEY;
+export const GGMAPS_URL = import.meta.env.VITE_GGMAPS_URL;
