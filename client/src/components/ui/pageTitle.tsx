23ae10c86c70f14ecf15ecca2d2e684428e826df
Nghia La <leonghiacnn@gmail.com>
2025-02-11 19:25:20 +0000

    Modify the styles API

--- client/src/components/ui/pageTitle.tsx
+++ client/src/components/ui/pageTitle.tsx
@@ -1,16 +1,37 @@
+import { cn } from "@/utils/styling";
 import React from "react";
 
-interface PageTitleProps extends React.ComponentPropsWithoutRef<"div"> {
+interface PageTitleProps
+  extends Omit<React.ComponentPropsWithoutRef<"div">, "className"> {
   title: string;
+  classNames?: {
+    root?: string;
+    text?: string;
+    dot?: string;
+  };
 }
 
 const PageTitle = React.forwardRef<HTMLDivElement, PageTitleProps>(
-  ({ title, ...props }, ref) => (
-    <div ref={ref} className="flex gap-2 sm:gap-1" {...props}>
-      <h1 className="text-4xl sm:text-lg font-bold sm:font-semibold text-white shrink-0">
+  ({ title, classNames, ...props }, ref) => (
+    <div
+      ref={ref}
+      className={cn("flex gap-2 sm:gap-1", classNames?.root)}
+      {...props}
+    >
+      <h1
+        className={cn(
+          "text-4xl sm:text-lg font-bold sm:font-semibold text-white shrink-0",
+          classNames?.text
+        )}
+      >
         {title}
       </h1>
-      <span className="inline-block translate-y-6 sm:translate-y-[15px] size-[0.625rem] sm:size-[0.375rem] bg-primary-500 rounded-full"></span>
+      <span
+        className={cn(
+          "inline-block translate-y-6 sm:translate-y-[15px] size-[0.625rem] sm:size-[0.375rem] bg-primary-500 rounded-full",
+          classNames?.dot
+        )}
+      />
     </div>
   )
 );
