9944b137995b9a77adeb8f519e2d2e9eaab2d571
Nghia La <leonghiacnn@gmail.com>
2025-02-26 12:04:14 +0000

    Create Ride model

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RidesController } from './rides/rides.controller';
+import { PlacesController } from './places/places.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, RidesController],
+  controllers: [AppController, RidesController, PlacesController],
   providers: [AppService],
 })
 export class AppModule {}
--- api/src/rides/domain/ride.ts
+++ api/src/rides/domain/ride.ts
@@ -1,7 +1,38 @@
 import { CarshareService } from 'src/utils/types/carshare-service';
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
+@Entity()
 export class Ride {
-  id: string;
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar', length: 129 })
+  rideId: string;
+
+  @Column({ type: 'timestamp with time zone' })
   pickupTime: Date;
+
+  @Column({ type: 'smallint' })
   service: CarshareService;
+
+  @Column({ type: 'smallint' })
+  numbersOfPassengers: number;
+
+  @Column({ type: 'integer' })
+  distanceInMeters: number;
+
+  @Column({ type: 'integer' })
+  estimatedDurationInSeconds: number;
+
+  @Column({ type: 'integer' })
+  actualDurationInSeconds: number | null;
+
+  @Column({ type: 'money' })
+  fare: number;
+
+  @Column({ type: 'varchar', length: 129 })
+  pickupPlaceId: string;
+
+  @Column({ type: 'varchar', length: 129 })
+  destinationPlaceId: string;
 }
